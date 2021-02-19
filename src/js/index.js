deca74a2bb1b8a8702b1d6d72cbe03ead74387ea
Michał Buda <michalbuda999@gmail.com>
2021-02-19 19:47:42 +0100

    added some hovers animation, bug on firefox browser fixed

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,12 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
-
-/* place your code below */
-
 console.log('Hi there 👋');
 
 const body = document.querySelector(".body");
 const hamburgerOpen = document.querySelector(".hamburger__js");
 const hambNav = document.querySelector(".navigation__hamburger");
 
+const logo = document.querySelector('.logo__js');
 const products = document.querySelector('.navigation__products');
 const about = document.querySelector('.navigation__about');
 const testimonials = document.querySelector('.navigation__testimonials');
@@ -47,24 +42,8 @@ contact.addEventListener('click', ()=>{
     body.classList.toggle('locked')
 })
 
-// const body = document.querySelector(".body");
-
-// const hamburger = document.querySelector('.hamburger');
-// const hamburgerMenu = document.querySelector('.navigation__hamburger');
-
-
-
-// hamburger.addEventListener('click', (e) =>{
-//     body.classList.toggle('locked');
-//     hamburger.classList.toggle('is-active');
-//     hamburgerMenu.classList.toggle('navigation__hamburger--visible');
-// })
-
-// products.addEventListener('click', (e) => {
-//     console.log('kliknales kurwa jego mac')
-//     body.classList.remove('locked');
-//     hamburger.classList.remove('is-active');
-//     hamburgerMenu.classList.remove('navigation__hamburger--visible');
-
-//     body.classList.remove('locked');
-// })
+logo.addEventListener('click', ()=>{
+    hamburgerOpen.classList.toggle('is-active');
+    hambNav.classList.toggle('navigation__hamburger--visible');
+    body.classList.toggle('locked')
+})
