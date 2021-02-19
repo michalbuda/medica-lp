5ad3f50c3f8e01c89e7e0ebe826101697d94b6dd
Michał Buda <michalbuda999@gmail.com>
2021-02-19 05:01:35 +0100

    added hamburger menu

--- src/js/index.js
+++ src/js/index.js
@@ -8,8 +8,42 @@ import '../scss/main.scss';
 
 console.log('Hi there 👋');
 
-const hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener('click', (e) =>{
-    hamburger.classList.toggle('is-active')
+const hamburgerOpen = document.querySelector(".hamburger__js");
+const hambNav = document.querySelector(".navigation__hamburger");
+
+const hamburgerItem = document.querySelector('.navigation__hamburger--list-item');
+
+hamburgerOpen.addEventListener('click', (e) => {
+    hamburgerOpen.classList.toggle('is-active');
+    hambNav.classList.toggle('navigation__hamburger--visible');
 })
+
+hamburgerItem.addEventListener('click', (e)=>{
+    console.log('xddd')
+})
+
+// const body = document.querySelector(".body");
+
+// const hamburger = document.querySelector('.hamburger');
+// const hamburgerMenu = document.querySelector('.navigation__hamburger');
+
+// const products = document.querySelector('.hamburger__products');
+// const about = document.querySelector('.hamburger__about');
+// const testimonials = document.querySelector('.hamburger__testimonials');
+// const contact = document.querySelector('.hamburger__contact');
+
+// hamburger.addEventListener('click', (e) =>{
+//     body.classList.toggle('locked');
+//     hamburger.classList.toggle('is-active');
+//     hamburgerMenu.classList.toggle('navigation__hamburger--visible');
+// })
+
+// products.addEventListener('click', (e) => {
+//     console.log('kliknales kurwa jego mac')
+//     body.classList.remove('locked');
+//     hamburger.classList.remove('is-active');
+//     hamburgerMenu.classList.remove('navigation__hamburger--visible');
+
+//     body.classList.remove('locked');
+// })
